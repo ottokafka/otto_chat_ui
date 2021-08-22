2259a53d6f8bf8252484287ddff0aff2dbd5525e
otto <otto@elfo.com>
2021-08-22 19:45:24 +0800

    added dotenv file

--- lib/config.dart
+++ lib/config.dart
@@ -1,31 +1,39 @@
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-// delete this token
-// var token =
-//     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im90dG8iLCJpc3MiOiJtYWNjaGF0LmNvbSJ9.iFE0KK3QByXFDzgjysJ6GNSUNUZnftE24F_IV4kYgH8";
+var websocketURL = dotenv.env['WEBSOCKET_URL'];
+var url = dotenv.env['URL'];
+var port = dotenv.env['PORT'];
 
 class Config {
   // in web use localhost but in android use 10.0.2.2
   // ios work with 127.0.0.1
 
-  // Websocket server IP and port for chat feature
-  static String websocketIP = 'ws://localhost:4000/socket';
+  // DEV Websocket server IP and port for chat feature
+  // static String websocketIP = 'ws://localhost:4000/socket';
+
+  // Prod raspberry pi server
+  static String websocketIP = "ws://$websocketURL";
+
   // static int websocketPort = 4000;
 
-  // Main server URL and Port
-  static String url = "localhost" + ":";
-  static String port = "4000";
+  // // DEV Main server URL and Port
+  // static String url = "localhost" + ":";
+  // static String port = "4000";
+
+  // Prod Main server URL and Port
 
   // Add a contact
-  static Uri addContactURL = Uri.parse("http://localhost:4000/addcontact");
+  static Uri addContactURL = Uri.parse("http://$url:$port/addcontact");
 
-  static Uri loginURL = Uri.parse("http://localhost:4000/signin");
-  static Uri signupURL = Uri.parse("http://localhost:4000/create");
+  static Uri loginURL = Uri.parse("http://$url:$port/signin");
+  static Uri signupURL = Uri.parse("http://$url:$port/create");
 }
 
 // initailize database for app
 // contacts
 void initailizeApp() async {
+  await dotenv.load(fileName: ".env");
   SharedPreferences prefs = await SharedPreferences.getInstance();
   var list = prefs.getStringList("contacts");
   print(list);
